9b78085708048687cff33bb4431e567d3c5ce9c3
Justin F <justinafriedlander@gmail.com>
2025-01-19 05:17:08 -0500

    Done

--- main.c
+++ main.c
@@ -62,7 +62,6 @@ CategoryNode * createCategoryNode(char * cardName) {
 
 // Get pointer to key in binary tree
 CategoryNode * getNode(CategoryNode * key, char * cardName) {
-
 	if (key == NULL) return NULL;
 	int compare = strcmp(key -> name, cardName);
 	if (*(key -> name) == *cardName) return key; // Only need to check the first letter
@@ -81,25 +80,26 @@ CategoryNode * initializeBoard() {
 	rootNode -> right -> left = createCategoryNode("puddings");
 	rootNode -> right -> right = createCategoryNode("tempura");
 	rootNode -> right -> right -> right = createCategoryNode("wasabi");
-
+	
 	return rootNode;
 	
 }
 
 // Create linked list nodes for the board hash table
 void createCardNode(CategoryNode * board, char * card) {
-	
-
 
 	// Make a node of the linked list with name = card
 	CardNode * node = malloc(sizeof(CardNode));
 	node -> name = malloc(sizeof(card) + 1);
 	strcpy(node -> name, card);
-	// category points to the key in the board hash table
+
+	// Category points to the key in the board hash table
 	CategoryNode * category = getNode(board, card);
-	// next points to the first element in the linked list
+	
+	// Next points to the first element in the linked list
 	node -> next = category -> cardList;
-	// node becomes the new first element of the linked list
+	
+	// Node becomes the new first element of the linked list
 	category -> cardList = node;
 }
 
@@ -114,7 +114,7 @@ int getNumCards(CategoryNode * board, char * card) {
 	return count;
 }
 
-// Initialize deck of cards. Returns a list of strings
+// Initialize deck of cards. Returns a list of pointers to strings
 char ** generateCards() {
 	char ** cards = malloc(TOTAL_CARDS * sizeof(char *));
 	int j=0;
@@ -183,42 +183,34 @@ char ** generateCards() {
 
 // Pick a random number out of range numbers
 int randomNum(int range) {
-	return (int)(rand() % (range));
+	return (int)(rand() % range);
 }
 
 
 // Play of one round
-void playRound(Player players[], int numPlayers, char ** cards) {
-
-
-
+void playRound(Player * players, int numPlayers, char ** cards) {
 	int sizeHand = 12 - numPlayers;
 	int index = 0;
+
 	// Pick starting hands
 	for (int i=0; i<numPlayers; i++) {
 		for (int j=0; j<sizeHand; j++) {
 				do index = randomNum(TOTAL_CARDS); while (strcmp(cards[index], "used") == 0); // Keep getting new random cards until it is not "used"
-				players[i].hand[j] = malloc(sizeof(cards[index]) +  1);
 				strcpy(players[i].hand[j], cards[index]);
-				strcpy(cards[index], "used"); // Nullify the pointer to the string
+				strcpy(cards[index], "used");
 		}
 	}
 
-	for (int i=0; i<numPlayers; i++){
-	for (int j=0; j<sizeHand; j++){
-	printf("Player %d's hand: %s\n", i, players[i].hand[j]);
-	}printf("\n\n");
-	}
 	// Loop through the turns
 	for (int i=0; i<sizeHand; i++) {
 		// Place down a random card from deck
 		for (int j=0; j<numPlayers; j++) {
 			do index = randomNum(sizeHand); while (strcmp(players[j].hand[index], "used") == 0);
 			createCardNode(players[j].board, players[j].hand[index]);
-			players[j].hand[index] = "used";
+			strcpy(players[j].hand[index], "used");
 		}
 
-		// Player 1 gets 0's hand, 2 gets 1's hand, etc. Use Player 0 as a buffer
+		// Swap hands using Player 0 as a buffer
 		char ** tmpHand;
 		for (int j=1; j<numPlayers; j++) {
 			tmpHand = players[0].hand;
@@ -236,10 +228,9 @@ void playRound(Player players[], int numPlayers, char ** cards) {
 	Note: I am not considering Chopsticks in the point-scoring or game playing
 */
 void calculatePoints(Player * player) {
-	printf("Player pointer: %p\n", player);
 	int total = 0;
 	CategoryNode * board = player -> board;
-	
+
 	total += 5 * (int)(getNumCards(board, "tempura")/2);
 	
 	total += 10 * (int)(getNumCards(board, "sashimi")/3);
@@ -247,7 +238,7 @@ void calculatePoints(Player * player) {
 	// 0.5numDumplings^2 + 0.5numDumplings represents the distribution of dumpling scores. This avoids if - else chain
 	int numDumplings = getNumCards(board, "dumplings");
 	total += 0.5 * (numDumplings * numDumplings + numDumplings);
-
+	
 	// Increase the points of however many of the highest scoring nigiri there are. Overflowing wasabi move to the next highest scoring nigiri
 	int numWasabi = getNumCards(board, "wasabi");
 	int numNigiriSquid = getNumCards(board, "nigiri squid");
@@ -262,19 +253,17 @@ void calculatePoints(Player * player) {
 
         total += numNigiriEgg + (numNigiriEgg * (numWasabi >= numNigiriEgg) + numWasabi * (numWasabi < numNigiriEgg)) * 2;
 
+	// Update player's member variables
 	player -> points += total;
 	player -> numMakis = getNumCards(board, "maki rolls three") * 3 + getNumCards(board, "maki rolls two") * 2 + getNumCards(board, "maki rolls one");
 	player -> numPuddings += getNumCards(board, "pudding");
-	printf("Num Puddings Real: %d\n", getNumCards(board, "pudding"));
-	printf("Num Puddings Board: %d\n", player -> numPuddings);
-	//printf("Player maki pointer: %p\nNUMBER OF MAKIS: %d\n", &player.numMakis, player.numMakis);
 }
 
 // Merge lists (part of merge sort algorithm)
 int (* merge(int (* left)[2], int leftSize, int (* right)[2], int rightSize))[2]{
 	int l = 0, r = 0;
-	int (* sortedList)[2] = malloc(leftSize + rightSize * 2 * sizeof(* sortedList));
-	
+	int (* sortedList)[2] = malloc((leftSize + rightSize) * sizeof(* sortedList));
+
 	// Merge left and right from biggest to smallest
 	while (l < leftSize && r < rightSize) {
 		if (left[l][1] > right[r][1]) {
@@ -288,7 +277,7 @@ int (* merge(int (* left)[2], int leftSize, int (* right)[2], int rightSize))[2]
 		}
 	}
 	
-	// When one list runs out of numbers, ensure the other list's numbers are not lost
+	// When one list runs out of numbers, fill the rest of the sorted list with the other list's numbers
 	for (; l<leftSize; l++) {
 		sortedList[l+r][0] = left[l][0];
 		sortedList[l+r][1] = left[l][1];
@@ -297,15 +286,25 @@ int (* merge(int (* left)[2], int leftSize, int (* right)[2], int rightSize))[2]
 		sortedList[l+r][0] = right[r][0];
 		sortedList[l+r][1] = right[r][1];
 	}
-
+	
+	// Free the sorted lists from the prior recursive run
+	free(left);
+	free(right);
+	
 	return sortedList;
 
 }
 
 // Merge sort main function
 int (* sort(int (* list)[2], int left, int right))[2] {
+
+	// Base case
 	if (left >= right - 1) {
-		return list;
+		int (* sizeOneList)[2] = malloc(1 * sizeof(sizeOneList));
+		sizeOneList[0][0] = list[left][0];
+		sizeOneList[0][1] = list[left][1];
+
+		return sizeOneList;
 	}
 
 	int mid = (left + right) / 2;
@@ -317,12 +316,11 @@ int (* sort(int (* list)[2], int left, int right))[2] {
 
 // Free cards
 void freeCards (char ** cards) {
-	/*
+	
 	for (int i=0; i<TOTAL_CARDS; i++) {
-		printf("Card: %s\nPointer: %p\n", cards[i], *cards[i]);
-		free(*cards[i]);
+		free(cards[i]);
 	}
-	*/
+	
 	free(cards);
 }
 
@@ -349,73 +347,66 @@ void freeNodes(CategoryNode * key) {
 // Free player's board
 void freeBoard(CategoryNode * board) {
 	freeNodes(board);
-	printf("Key: %s\n", board -> name);
 	if (board -> left) {
-		freeBoard(board -> left);;
-		//free(board -> left);
+		freeBoard(board -> left);
 	}
 	if (board -> right) {
 		freeBoard(board -> right);
-		 //free(board -> right);
 	}
-
-	free(board -> name);
-	//free(&board);
+	free(board);
 }
 
 // Free players
 void freePlayers(Player * players, int numPlayers) {
 	for (int i=0; i<numPlayers; i++) {
 		for (int j=0; j<(12-numPlayers); j++) {
-			//free(players[i].hand[j]);
+			free(players[i].hand[j]);
 		}
+
 		free(players[i].hand);
-
-		freeBoard(players[i].board); // Note: this requires a separate function for recursion
-		//free(players[i].board);
 	}
-
-	//free(players);
 }
 
 void main() {
+	// For randomness function
+	srand(time(0));
+
 	// Get number of players
 	int numPlayers;
 	printf("How many players are there? (2-5)\n");
 	scanf("%d", &numPlayers);
 
 	int handSize = 12 - numPlayers;
-
+	
+	// Initialize 2d arrays for numMakis and numPuddings
 	int (* numMakis)[2] = malloc(numPlayers * sizeof(* numMakis));
 	int (* numPuddings)[2] = malloc(numPlayers * sizeof(* numPuddings));
-
+	
 	// Initialize variables for players
 	Player players[numPlayers];
 	for (int i=0; i<numPlayers; i++) {
 		players[i].points = 0;
-		players[i].hand = malloc(handSize * sizeof(char *));
+		players[i].hand = malloc(handSize * sizeof(players[i].hand));
+		for (int j=0; j<handSize; j++) {
+			players[i].hand[j] = malloc(MAX_CARD_NAME * sizeof(char));
+		}
 		players[i].board = initializeBoard();
 		players[i].numPuddings = 0;
 	}
-	
+
 	// Make list of cards
 	char ** cards = generateCards();
-       	
+
 	// Play rounds
 	for (int i=0; i<3; i++) {
 		// Play the round. Note no return value because the function directly updates the data at the memory address
-		printf("Still on\n");
 		playRound(players, numPlayers, cards);
-
-
+		
 		// Calculate points for the round
 		for (int j=0; j<numPlayers; j++) {
-			printf("Player pointer before: %p\n", &players[j]);
 			calculatePoints(& players[j]);
-			printf("Player pointer after: %p\n", &players[j]);
 			
-			// Maki calculations
-			printf("After maki pointer: %p\t%d\n", &players[j].numMakis, players[j].numMakis);
+			// Update Makis
 			numMakis[j][0] = j;
 			numMakis[j][1] = players[j].numMakis;
 
@@ -425,13 +416,13 @@ void main() {
 		}
 
 		// Maki point distributing
-		for (int j=0; j<numPlayers; j++){
-			printf("Player: %d\nMakis: %d\n\n", numMakis[j][0],numMakis[j][1]);}
-		numMakis = sort(numMakis, 0, numPlayers-1);
+		int (* numMakisSorted)[2] = sort(numMakis, 0, numPlayers-1);
+		free(numMakis);
+		numMakis = numMakisSorted;
 		int makiTieSize = 1;
 		int makiSecondTieSize = 0;
 
-		// Calculate ties
+		// Calculate Maki ties
 		for (int j=1; j<numPlayers; j++) {
 			if (numMakis[j][1] == numMakis[0][1]) makiTieSize++;
 			if (numMakis[j][1] != numMakis[0][1] && numMakis[j][1] == numMakis[makiTieSize+1][1]) makiSecondTieSize++;
@@ -442,16 +433,19 @@ void main() {
 			players[numMakis[j][0]].points += (int) (6 / makiTieSize);
 		}
 		for (int j=makiTieSize; j<(makiTieSize + makiSecondTieSize); j++) {
-			players[numMakis[j][0]].points += (int) (3 / makiTieSize);
+			players[numMakis[j][0]].points += (int) (3 / makiSecondTieSize);
 		}
 		
 		
 		// Pudding point distributing
 		if (!(3-i)) {
-			printf("Over\n");
-			numPuddings = sort(numPuddings, 0, numPlayers-1);
+			int (* numPuddingsSorted)[2] = sort(numPuddings, 1, numPlayers-1);
+			free(numPuddings);
+			numPuddings = numPuddingsSorted;
+
 			int mostPuddingsTieSize = 0;
 			int leastPuddingsTieSize = 0;
+			
 			// Calculating ties
 			for (int j=0; j<numPlayers; j++) {
 				if (numPuddings[j][1] == numPuddings[0][1]) mostPuddingsTieSize++;
@@ -464,17 +458,22 @@ void main() {
 			}
 			if (numPlayers > 2) {
 				for (int j=0; j<leastPuddingsTieSize; j++) {
-					players[numPuddings[numPlayers-j][0]].points -= (int) (6 / mostPuddingsTieSize);
+					players[numPuddings[numPlayers-j-1][0]].points -= (int) (6 / leastPuddingsTieSize);
 				}
 			}
+
+			// Free board on last run of rounds loop to avoid munmap_chunk error
+			for (int j=0; j<numPlayers; j++) {
+				freeBoard(players[j].board);
+			}
+			
 		} else {
 			// Round updates on all but the final (3rd) round
+			printf("\nRound %d\n", i+1);
 			for (int j=1; j<numPlayers+1; j++) {
-				printf("Round %d\n", i+1);
 				printf("Player %d: %d points\t%d Puddings\n", j, players[j-1].points, players[j-1].numPuddings);
 			}
 		}
-
 	}
 
 	// Determine winner
@@ -487,20 +486,14 @@ void main() {
 			winnerIndex = i;
 		}
 	}
-
-	printf("\nWinner is Player %d\n", winnerIndex+1);
+	
+	printf("\nWinner is Player %d! Congratulations!\n\n", winnerIndex+1);
 	
 	// Free memory
-	//freeInts(numMakis, numPlayers);
-	//freeInts(numPuddings, numPlayers);
 	free(numMakis);
-	printf("Makis Free'd\n");
 	free(numPuddings);
-	printf("Puddings Free'd\n");
 	freeCards(cards);
-	printf("Cards Free'd\n");
 	freePlayers(players, numPlayers);
-	printf("Players Free'd");
 }
 
 
